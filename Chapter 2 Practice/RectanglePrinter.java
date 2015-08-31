70b08c4bb44d7031b69b4d258771bddf9b130884
anavit15 <atekriwal@NN121-HA-01.HS.sd203.org>
2015-08-31 10:25:39 -0500

    Chapter 2 Practice with Rectangle and Color

---
+++ Chapter 2 Practice/RectanglePrinter.java
@@ -0,0 +1,37 @@
+/*import java.awt.Rectangle;
+
+public class RectanglePrinter
+{
+   public static void main(String[] args)
+   {
+      Rectangle r1 = new Rectangle(0, 0, 100, 50);
+      Rectangle r2 = new Rectangle(0, 0, 100, 50);
+      r2.grow(10,20);
+      System.out.println(r1);
+      System.out.println(r2);
+   }
+}
+
+import java.awt.Rectangle;
+public class RectanglePrinter
+{
+    public static void main (String[] args)
+    {
+        Rectangle box=new Rectangle(0,0,0,0);
+        box.grow(100,50);
+        box.setLocation(100,50);
+        System.out.println(box);
+    }
+}
+*/
+import java.awt.Rectangle;
+public class RectanglePrinter
+{
+    public static void main(String[] args)
+    {
+        Rectangle box=new Rectangle(5,10,20,30);
+        box.add(0,0);
+        System.out.println(box);
+    }
+}
+        
